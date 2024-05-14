3847aabd35ebb0de770a9e99b4e2e06dfc4e6cd2
Gerson Passos <gersonpassos27@gmail.com>
2024-05-13 21:22:39 -0300

    adicionando links

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,18 +5,18 @@ export default function Content(){
     <section className="w-full flex-1 px-72 flex items-center">
       <div className="h-36 w-full flex items-center justify-center border border-white rounded-md">
         <div className='flex items-center justify-center gap-4'>
-          <a href="" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
+          <a href="https://www.instagram.com/gersinwq" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
             <Instagram />
           </a>
-          <a href="" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
+          <a href="https://www.youtube.com/channel/UCWFrJWuRMXoc8YyMRg0JNjw" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
             <Youtube />
           </a>
-          <a href="" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
+          <a href="https://github.com/gerson-passos" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
             <Github />
           </a>
-          <a href="" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
+          <button className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
             <Mail />
-          </a>
+          </button>
         </div>
       </div>
     </section>
