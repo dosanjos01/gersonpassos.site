e9ecd83a6ce02dae32d7f2c5cfe3884b2365dbcb
Gerson Passos <gersonpassos27@gmail.com>
2024-05-24 20:27:53 -0300

    escurecendo a borda do container das redes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import {Instagram, Youtube, Github, Mail, MessageCircle} from 'lucide-react'
 export default function Content(){
   return (
     <section className="w-full flex-1 px-72 flex items-center">
-      <div className="h-36 w-full flex items-center justify-center border border-white rounded-md">
+      <div className="h-36 w-full flex items-center justify-center border border-zinc-800 rounded-md">
         <div className='flex items-center justify-center gap-4'>
           <a href="https://www.instagram.com/gersinwq" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
             <Instagram />
@@ -14,7 +14,7 @@ export default function Content(){
           <a href="https://github.com/gerson-passos" className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
             <Github />
           </a>
-          <button className='border border-zinc-800 p-2 rounded-md hover:opacity-50 transition-all'>
+          <button className='border border-zinc-800 p-2 rounded-md hovesr:opacity-50 transition-all'>
             <Mail />
           </button>
         </div>
